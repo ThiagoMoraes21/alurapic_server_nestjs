f55fc924b2a76efc7f28ae97e63e9c85b87a1b86
Thiago Moraes <thiago4x70@gmail.com>
2021-07-22 10:55:45 -0300

    add class validations to the user entity

--- src/usuario/entity/usuario.entity.ts
+++ src/usuario/entity/usuario.entity.ts
@@ -1,8 +1,32 @@
+import { IsDate, IsEmail, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
 export class Usuario {
+    @IsNumber()
     id: number;
+
+    @IsNotEmpty({
+        message: 'nomeDeUsuario é obrigatório.'
+    })
+    @IsString({
+        message: 'nomeDeUsuario precisa ser uma string.'
+    })
     nomeDeUsuario: string;
+
+    @IsEmail({}, {
+        message: 'email precisar ser um endereço de email válido.'
+    })
     email: string;
+
+    @IsNotEmpty({
+        message: 'senha é obrigatório.'
+    })
     senha: string;
+
+    @IsNotEmpty({
+        message: 'nomeComplete é obrigatório'
+    })
     nomeCompleto: string;
+
+    @IsDate()
     dataDeEntrada: Date;
 }
